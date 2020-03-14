fc687f9cf760ccca6cb689eaf21d0f3644b46d7e
George Hazan <ghazan@miranda.im>
2020-03-14 10:00:46 +0300

    merge with trunk

--- plugins/Scriver/src/msgdialog.cpp
+++ plugins/Scriver/src/msgdialog.cpp
@@ -273,7 +273,6 @@ bool CMsgDialog::OnInitDialog()
 
 	SendMessage(m_hwnd, DM_CHANGEICONS, 0, 0);
 
-	m_message.SetReadOnly(false);
 	m_message.SendMsg(EM_SETLANGOPTIONS, 0, (LPARAM)m_message.SendMsg(EM_GETLANGOPTIONS, 0, 0) & ~IMF_AUTOKEYBOARD);
 	m_message.SendMsg(EM_SETEVENTMASK, 0, ENM_MOUSEEVENTS | ENM_KEYEVENTS | ENM_CHANGE | ENM_REQUESTRESIZE);
 	if (m_hContact && m_szProto) {
@@ -517,16 +516,15 @@ void CMsgDialog::onClick_Ok(CCtrlButton *pButton)
 void CMsgDialog::onClick_UserMenu(CCtrlButton *pButton)
 {
 	if (GetKeyState(VK_SHIFT) & 0x8000) { // copy user name
-		char buf[128];
-		GetContactUniqueId(buf, sizeof(buf));
-		if (!OpenClipboard(m_hwnd) || !mir_strlen(buf))
+		ptrW id(Contact_GetInfo(CNF_UNIQUEID, m_hContact, m_szProto));
+		if (!OpenClipboard(m_hwnd) || !mir_wstrlen(id))
 			return;
 
 		EmptyClipboard();
-		HGLOBAL hData = GlobalAlloc(GMEM_MOVEABLE, mir_strlen(buf) + 1);
-		mir_strcpy((LPSTR)GlobalLock(hData), buf);
+		HGLOBAL hData = GlobalAlloc(GMEM_MOVEABLE, 2*mir_wstrlen(id) + 1);
+		mir_wstrcpy((LPWSTR)GlobalLock(hData), id);
 		GlobalUnlock(hData);
-		SetClipboardData(CF_TEXT, hData);
+		SetClipboardData(CF_UNICODETEXT, hData);
 		CloseClipboard();
 	}
 	else {
@@ -1149,10 +1147,9 @@ INT_PTR CMsgDialog::DlgProc(UINT msg, WPARAM wParam, LPARAM lParam)
 	case DM_CLISTSETTINGSCHANGED:
 		if (wParam == m_hContact && m_hContact && m_szProto) {
 			DBCONTACTWRITESETTING *cws = (DBCONTACTWRITESETTING *)lParam;
-			char idbuf[128], buf[128];
-			GetContactUniqueId(idbuf, sizeof(idbuf));
-			mir_snprintf(buf, Translate("User menu - %s"), idbuf);
-			SendDlgItemMessage(m_hwnd, IDC_USERMENU, BUTTONADDTOOLTIP, (WPARAM)buf, 0);
+			wchar_t buf[128];
+			mir_snwprintf(buf, TranslateT("User menu - %s"), ptrW(Contact_GetInfo(CNF_UNIQUEID, m_hContact, m_szProto)).get());
+			SendDlgItemMessage(m_hwnd, IDC_USERMENU, BUTTONADDTOOLTIP, (WPARAM)buf, BATF_UNICODE);
 
 			if (cws && !mir_strcmp(cws->szModule, m_szProto) && !mir_strcmp(cws->szSetting, "Status"))
 				m_wStatus = cws->value.wVal;
--- plugins/TabSRMM/src/msgdialog.cpp
+++ plugins/TabSRMM/src/msgdialog.cpp
@@ -567,7 +567,6 @@ bool CMsgDialog::OnInitDialog()
 
 	m_message.SendMsg(EM_SETEVENTMASK, 0, ENM_REQUESTRESIZE | ENM_MOUSEEVENTS | ENM_SCROLL | ENM_KEYEVENTS | ENM_CHANGE);
 	m_message.SendMsg(EM_SETMARGINS, EC_LEFTMARGIN | EC_RIGHTMARGIN, MAKELONG(3, 3));
-	m_message.SetReadOnly(false);
 
 	m_bActualHistory = M.GetByte(m_hContact, "ActualHistory", 0) != 0;
 
@@ -1969,11 +1968,15 @@ LRESULT CMsgDialog::WndProc_Message(UINT msg, WPARAM wParam, LPARAM lParam)
 		if (isChat()) {
 			// tab-autocomplete
 			if (wParam == VK_TAB && !isCtrl && !isShift) {
+				// if tab acts as a key pressing, simply do nothing
+				if (PluginConfig.m_bAllowTab)
+					break;
+
 				m_message.SendMsg(WM_SETREDRAW, FALSE, 0);
 				bool fCompleted = TabAutoComplete();
 				m_message.SendMsg(WM_SETREDRAW, TRUE, 0);
 				RedrawWindow(m_message.GetHwnd(), nullptr, nullptr, RDW_INVALIDATE);
-				if (!fCompleted && !PluginConfig.m_bAllowTab) {
+				if (!fCompleted) {
 					if ((GetSendButtonState(GetHwnd()) != PBS_DISABLED))
 						SetFocus(m_message.GetHwnd());
 					else
